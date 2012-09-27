56bb73428e60eea10cd08377c4c91c8e6f094b62
Lupo Montero <lupo@e-noise.com>
2012-09-27 12:01:20 -0500

    tweaking...

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var
   express = require('express'),
+  port = process.argv[1] || process.env.OPENSHIFT_INTERNAL_PORT || 8080,
+  ip = process.argv[2] || process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1',
   app = express();
 
 app.configure(function () {
   app.use(express.static(__dirname + '/public'));
 });
 
-app.listen(process.env.OPENSHIFT_INTERNAL_PORT, process.env.OPENSHIFT_INTERNAL_IP);
+app.listen(port, ip);
