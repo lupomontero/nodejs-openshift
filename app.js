4cc1803dc939432df03b60a36ac980bd4598dd93
Lupo Montero <lupomontero@gmail.com>
2014-11-11 13:05:50 -0500

    upgrade deps and clean up styles

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
-var
-  express = require('express'),
-  port = process.argv[2] || 8080,
-  ip = process.argv[3] || '127.0.0.1',
-  app = express();
 
-app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'hbs');
-  app.use(express.static(__dirname + '/public'));
-});
+var express = require('express');
+var port = process.argv[2] || 8080;
+var ip = process.argv[3] || '127.0.0.1';
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res, next) {
   res.render('index', { title: 'A test app', node_version: process.version });
 });
 
-app.listen(port, ip);
-console.log('nodejs ' + process.version + ' server listening on ' + ip + ':' + port);
+app.listen(port, ip, function () {
+  console.log('nodejs ' + process.version + ' server listening on ' + ip + ':' + port);
+});
